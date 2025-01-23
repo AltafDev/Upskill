f0ccd7dfbb6c64f5443bf7ab8966d84c29a4cc8f
AltafDev <altafkhan1743@gmail.com>
2025-01-23 08:20:54 -0800

    fsfs

---
+++ src/Lib/Database/DB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+let intialized=false
+
+export const Database =async()=>{
+    mongoose.set("strictQuery",true)
+    if(intialized){
+        console.log("Database is already conected")
+        return
+    }
+    try {
+        await mongoose.connect(process.env.Mongodb_URL,{
+            dbName: 'altafkhan1743',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Database is connected")
+    } catch (error) {
+        console.log("Database is not connected")
+    }
+}
