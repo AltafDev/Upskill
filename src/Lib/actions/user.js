480b38520bd38fe99ad694ac45da9fd284be3478
AltafDev <altafkhan1743@gmail.com>
2025-01-24 07:21:16 -0800

    fdsfsf

--- src/Lib/actions/user.js
+++ src/Lib/actions/user.js
@@ -1,31 +1,41 @@
 import { Database } from "../Database/DB";
 import User from "../modals/usermodal";
 
-export const CreateOrUpdaateUser =async(id,first_name,last_name,email_addresses,username)=>{
+
+export const CreateOrUpdateUser = async (
+    id,
+    first_name,
+    last_name,
+    image_url,
+    email_addresses
+  
+  ) => {
     try {
-        await Database()
-        const user= User.findOneAndUpdate(
-            {clekid,id},
-           {
-            $set:{
-                firstname:first_name,
-                lastname:last_name,
-                username:username,
-                email:email_addresses[0].email_address
-            }
-           },
-           {new:true,upsert:true}
-        )
-        return user
+      await Database();
+      const user = await User.findOneAndUpdate(
+        { clerkId: id },
+        {
+          $set: {
+            firstName: first_name,
+            lastName: last_name,
+            avatar: image_url,
+            email: email_addresses[0].email_address
+      
+          },
+        },
+        { new: true, upsert: true }
+      );
+      return user;
     } catch (error) {
-        console.log(error)
+      console.log('Error creating or updating user:', error);
     }
-}
-export const DeletUser=async(id)=>{
+  };
+  
+  export const DeleteUser = async (id) => {
     try {
-        await Database()
-    await User.findOneAndDelete({clerkid:id})
+      await Database();
+      await User.findOneAndDelete({ clerkId: id });
     } catch (error) {
-        console.log(error)
+      console.log('Error deleting user:', error);
     }
-}
+  };
